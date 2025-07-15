6f7fe4ae473a431836a4762faa078a0f3a22a341
PalomoIFCA <palomo@ifca.unican.es>
2025-07-15 12:54:25 +0200

    add crete link in place

--- QC.Sty/pyophidia/find_oph_workflows.py
+++ QC.Sty/pyophidia/find_oph_workflows.py
@@ -121,7 +121,8 @@ def cwl_converter(path):
     json_paths=[]
     for cwl_workflow in cwl_paths:
           print(cwl_workflow)
-          os.system("ln -s /usr/local/lib/python3.10/site-packages/pyophidia/utils/tasks/ tasks")
+          print("ln -s /usr/local/lib/python3.10/site-packages/pyophidia/utils/tasks/ "+os.path.dirname(cwl_workflow)+"tasks")
+          os.system("ln -s /usr/local/lib/python3.10/site-packages/pyophidia/utils/tasks/ "+os.path.dirname(cwl_workflow)+"tasks")
           work=ophexperiment.load_cwl(cwl_workflow,"--nthreads 5")
           #print('le worki ',work)
           with open(cwl_workflow+".json","w") as new_json:
