fdb04261d1f628520efe54c8f13520c35d4c354e
PalomoIFCA <palomo@ifca.unican.es>
2025-07-15 09:36:58 +0200

    added function

--- QC.Sty/pyophidia/find_oph_workflows.py
+++ QC.Sty/pyophidia/find_oph_workflows.py
@@ -109,6 +109,20 @@ def download(url):
     pathfile = ["downloaded_workflow.json"]
     return pathfile
 
+def cwl_converter(path):
+    ophexperiment = Experiment(
+        name="validation", author="user", abstract="validation test"
+    )
+    cwl_paths=find(".cwl", path)
+    json_paths=[]
+    for cwl_workflow in cwl_paths:
+          print(cwl_workflow)
+          work=ophexperiment.load_cwl(cwl_workflow,"--nthreads 5")
+          #print('le worki ',work)
+          with open(cwl_workflow+".json","w") as new_json:
+               json.dump(work,new_json)
+          json_paths.append(cwl_workflow+".json")    
+    return(cwl_paths,json_paths)
 
 def main():
     # get input arguments
@@ -126,6 +140,7 @@ def main():
     if args.args_path:
         res = evaluate_workflow_path(candid, args.args_path)
     else:
+        cwl_paths,json_paths=cwl_converter(args.path)
         res = evaluate_workflow_path(
             candid,
         )
