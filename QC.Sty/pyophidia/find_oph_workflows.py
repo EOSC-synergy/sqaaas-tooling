7ab7148876f2e2c9c27eb9aefd52679ac13159c0
PalomoIFCA <palomo@ifca.unican.es>
2025-09-01 11:28:45 +0200

    test presence module

--- QC.Sty/pyophidia/find_oph_workflows.py
+++ QC.Sty/pyophidia/find_oph_workflows.py
@@ -83,7 +83,7 @@ def evaluate_workflow_path(candidates, arguments={"filename": ["1", "historic"]}
             reasons_list.append(msg)
 
         results = {
-            "result": os.system('pyophidia.__file__')#passed
+            "result": pyophidia.__file__#passed
             "passed_list": passed_list+,
             "failed_list": failed_list,
             "reasons_list": reasons_list,
