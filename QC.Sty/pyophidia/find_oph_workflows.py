79704d7c86810b4f5d947b7ef1afc54f30e5d871
PalomoIFCA <palomo@ifca.unican.es>
2025-09-01 08:46:16 +0200

    comment cwl

--- QC.Sty/pyophidia/find_oph_workflows.py
+++ QC.Sty/pyophidia/find_oph_workflows.py
@@ -146,7 +146,7 @@ def main():
     if args.args_path:
         res = evaluate_workflow_path(candid, args.args_path)
     else:
-        cwl_paths,json_paths=cwl_converter(args.path)
+        #cwl_paths,json_paths=cwl_converter(args.path)
         res = evaluate_workflow_path(
             candid,
         )
