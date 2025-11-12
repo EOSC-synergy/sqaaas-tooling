ecd301ee08bfd98718718ece407374e7e0d50fce
PalomoIFCA <palomo@ifca.unican.es>
2025-11-12 13:11:36 +0100

    correct file location

--- QC.Sty/pynblint/pynblint.py
+++ QC.Sty/pynblint/pynblint.py
@@ -9,7 +9,7 @@ max_warnings=3 #max number of warnings per file before failure
 
 path=os.getcwd()
 print(path,os.listdir())
-with open('ls.txt','r') as file:
+with open('./ls.txt','r') as file:
    base_file_list = file.read().splitlines()
 
 actual_file_list= os.listdir()
