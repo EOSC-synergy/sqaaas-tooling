358f8e27b6412162ad99d3d4b689a265f1d9d721
PalomoIFCA <palomo@ifca.unican.es>
2026-02-25 14:29:47 +0100

    cleaning and formatting

--- QC.Sty/pynblint/pynblint.py
+++ QC.Sty/pynblint/pynblint.py
@@ -3,80 +3,90 @@ import json
 import os
 import sys
 
-#parameters
-max_warnings=3 #max number of warnings per file before failure
+# parameters
+max_warnings = 3  # max number of warnings per file before failure
 
-os.chdir('/')
-path=os.getcwd()
+os.chdir("/")
+path = os.getcwd()
 
-with open('/ls.txt','r') as file:
-   base_file_list = file.read().splitlines()
+with open("/ls.txt", "r") as file:
+    base_file_list = file.read().splitlines()
 
-actual_file_list= os.listdir()
+actual_file_list = os.listdir()
 
-final_file_list=list(set(actual_file_list)-set(base_file_list))
-
-
-path=final_file_list[0]
+final_file_list = list(set(actual_file_list) - set(base_file_list))
 
 
+path = final_file_list[0]
 
-#print('pynblint ' + path + ' -o report.json -S -q -y')
 
-os.system('pynblint ' + path + ' -o report.json -S -q -y') 
-with open('report.json','r') as file:
-   data = json.load(file)
-   
+# print('pynblint ' + path + ' -o report.json -S -q -y')
 
-results={}
+os.system("pynblint " + path + " -o report.json -S -q -y")
+with open("report.json", "r") as file:
+    data = json.load(file)
+
+
+results = {}
+
+
 def evaluate_json_out(lints):
-    
+
     passed = True
-    warnings_counter = 0 
+    warnings_counter = 0
     passed_list = []
     failed_list = []
     passed_reasons_list = []
     failed_reasons_list = []
-    
+
     for lint in lints:
-       notebook_name = lint['notebook_metadata']['notebook_name']
-       #print(lint.keys())
-       #print(lint['notebook_metadata'])
-       #print (len(lint['lints']))
-       warnings_counter += len(lint['lints'])
-       if len(lint['lints'])>max_warnings:
-          #print(lint['lints'])
-          failed_list.append(notebook_name)
-          failed_reasons_list.append({notebook_name:lint['lints']})
-       else:
-          
-          passed_list.append(notebook_name)
-          passed_reasons_list.append({notebook_name:lint['lints']})
-    
-    if warnings_counter > len(lints)*max_warnings:
-       passed = False
-    subcriterion_id='QC.Sty01'
-    subcriterion_description = 'Is the software product following a style standard for Jupyter Notebooks?'
-    subcriterion_hint= 'Check the following notebook files: '
+        notebook_name = lint["notebook_metadata"]["notebook_name"]
+        # print(lint.keys())
+        # print(lint['notebook_metadata'])
+        # print (len(lint['lints']))
+        warnings_counter += len(lint["lints"])
+        if len(lint["lints"]) > max_warnings:
+            # print(lint['lints'])
+            failed_list.append(notebook_name)
+            failed_reasons_list.append({notebook_name: lint["lints"]})
+        else:
+
+            passed_list.append(notebook_name)
+            passed_reasons_list.append({notebook_name: lint["lints"]})
+
+    if warnings_counter > len(lints) * max_warnings:
+        passed = False
+    subcriterion_id = "QC.Sty01"
+    subcriterion_description = (
+        "Is the software product following a style standard for Jupyter Notebooks?"
+    )
+    subcriterion_hint = "Check the following notebook files: "
     for archive in failed_list:
-        subcriterion_hint+= archive +', '
+        subcriterion_hint += archive + ", "
     subcriterion_valid = passed
     subcriterion_evidence = failed_reasons_list
-    subcriterion_requirement_level='RECOMMENDED'   
+    subcriterion_requirement_level = "RECOMMENDED"
     results = {
         "result": passed,
         "passed_list": passed_list,
         "failed_list": failed_list,
         "passed_reasons_list": passed_reasons_list,
         "failed_reasons_list": failed_reasons_list,
-        "criterion": 'QC.Sty',
-        "subcriterion" : [{'id': subcriterion_id, 'description': subcriterion_description, 'hint': subcriterion_hint, 'valid': subcriterion_valid, 'evidence': failed_reasons_list, 'requirement_level': subcriterion_requirement_level}],
-
+        "criterion": "QC.Sty",
+        "subcriterion": [
+            {
+                "id": subcriterion_id,
+                "description": subcriterion_description,
+                "hint": subcriterion_hint,
+                "valid": subcriterion_valid,
+                "evidence": failed_reasons_list,
+                "requirement_level": subcriterion_requirement_level,
             }
-    return(json.dumps(results))
+        ],
+    }
+    return json.dumps(results)
 
-res=evaluate_json_out(data['notebook_level_lints'])
+
+res = evaluate_json_out(data["notebook_level_lints"])
 
 print(res)
-
-
