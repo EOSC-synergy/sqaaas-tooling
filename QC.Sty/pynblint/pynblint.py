ab9246f078903c0b541443c7436031e2b6222fac
PalomoIFCA <palomo@ifca.unican.es>
2026-03-18 10:47:51 +0100

    solve error by removing non relevant output

--- QC.Sty/pynblint/pynblint.py
+++ QC.Sty/pynblint/pynblint.py
@@ -21,7 +21,7 @@ path = final_file_list[0]
 
 
 
-os.system("pynblint " + path + " -o report.json -S -q -y")
+os.system("pynblint " + path + " -o report.json -S -q -y 2> null.txt")
 with open("report.json", "r") as file:
     data = json.load(file)
 
